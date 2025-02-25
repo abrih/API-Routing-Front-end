efe84898630880cda5f0baec315786b2489bff90
Theo123-art <abrish.fikre@gmail.com>
2025-02-24 19:28:08 -0500

    style for youtube videos

--- src/Components/YoutubeVideos/YoutubeVideos.jsx
+++ src/Components/YoutubeVideos/YoutubeVideos.jsx
@@ -11,7 +11,7 @@ const Youtubevideos = () => {
   //  https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCE_M8A5yxnLfW0KghEeajjw&maxResults=10&order=date&key=AIzaSyAD8BxVYPU7RiLWQ7ResI2Wj-xkUBKqJSw'
   useEffect(() => {
     fetch(
-      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCE_M8A5yxnLfW0KghEeajjw&maxResults=10&order=date&key=AIzaSyAD8BxVYPU7RiLWQ7ResI2Wj-xkUBKqJSw"
+      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCE_M8A5yxnLfW0KghEeajjw&maxResults=9&order=date&key=AIzaSyAD8BxVYPU7RiLWQ7ResI2Wj-xkUBKqJSw"
     )
       .then((response) => response.json())
       .then((data) => {
@@ -27,20 +27,21 @@ const Youtubevideos = () => {
 
       {youtubevideo?.map((singlevideo) => {
         return (
-          <div key={singlevideo.id.videoId}>
+          <div className={styles.applevideo} key={singlevideo.id.videoId}>
             <a
               href={`https://www.youtube.com/watch?v=${singlevideo.id.videoId}`}
               target="_blank"
               rel="noopener noreferrer"
             >
               <img
+                className={styles.applevideo}
                 src={singlevideo.snippet.thumbnails.high.url}
                 alt={singlevideo.snippet.title}
               />
             </a>
             <p>{singlevideo.snippet.title}</p>
             <p>{singlevideo.snippet.description}</p>
-          </div>
+        </div>
         );
       })}
     </div>
